2c4341375c0f41ec79ca0aa31f82e661719ce745
Factory-KJC <shouta.sakamaki@outlook.com>
2024-07-14 13:34:54 +0900

    [Fix] Update n225mpf.py
    フォントファイルの指定と，ファイル名の変更を実施

---
+++ src/n225mpf.py
@@ -0,0 +1,31 @@
+import io
+#yfinanceをインポート
+import yfinance as yf
+#matplotlibのfont_managerとmplfinanceとdatetimeをインポート
+from matplotlib import font_manager as fm
+import mplfinance as mpf
+import datetime as dt
+
+def n225plot():
+
+    buffer = io.BytesIO()
+
+    # 取得するデータ詳細
+    name = '^N225'
+    today = dt.datetime.now()
+    start = today - dt.timedelta(days=1)
+    end = dt.datetime.now()
+    interval = '1m'
+
+    # データのダウンロード
+    df = yf.download(tickers=name, start=start, end=end, interval=interval)
+    df.head()
+
+    # スタイルの指定
+    font_path = '../fonts_and_licenses/NotoSansJP-VariableFont_wght.ttf'
+    fm.fontManager.addfont(font_path)
+    cs = mpf.make_mpf_style(base_mpf_style='yahoo', rc={"font.family":'Noto Sans JP'})
+
+    # プロットしてBytesIOとして返す
+    mpf.plot(df, type="candle", title='日経平均株価', ylabel='株価', figratio = (3, 2), volume=False, style=cs ,returnfig=True, closefig=True, savefig = buffer)
+    return buffer
